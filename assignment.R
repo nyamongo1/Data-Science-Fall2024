c1283b117e167965fab8121aa85015c1e1483f34
nyamongo1 <anyamongo@claflin.edu>
2024-09-18 17:58:22 +0200

    Add files via upload

---
+++ assignment.R
@@ -0,0 +1,17 @@
+#Author:Nyamongo,Date: AUGUST 26 2024, purppose: calculate correlation test
+#library("ggpbur")
+my_data<-mtcars
+#read mtcars dataset in R
+#calculate correlation value between my_data$wt, my_data$gear
+> result<- cor.test(my_data$wt,my_data$gear,method="pearson")
+> result
+ Pearson's product-moment correlation
+
+data:  my_data$wt and my_data$gear
+t = -3.9332, df = 30, p-value = 0.0004587
+alternative hypothesis: true correlation is not equal to 0
+95 percent confidence interval:
+ -0.7744638 -0.2944887
+sample estimates:
+      cor 
+-0.583287 
