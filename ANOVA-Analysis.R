5d9175fef47e05144dca0d7aeafd520b4fa0cd99
nyamongo1 <anyamongo@claflin.edu>
2024-09-23 16:57:46 +0200

    Update ANOVA-Analysis.R

--- ANOVA-Analysis.R
+++ ANOVA-Analysis.R
@@ -5,8 +5,8 @@
 #where can your input data be stored?
 #Local drive( external drive/USB,harddrive,server,cluster);cloud storage(amazon cloud,google cloud,apple icloud), HTML page( github)
 #import the dummy data from Gitup public repository in our Rprogram.
-#librar(dplyr)
-PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
+>library(dplyr)
+>PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
 #Reading the CSV file in R
 >df<-read.csv(PATH)
 >head(df)
