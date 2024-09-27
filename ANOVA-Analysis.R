9d1a11aac5acbfc17471a9b5e8a96d7490c5bf38
nyamongo1 <anyamongo@claflin.edu>
2024-09-27 13:02:12 +0200

    Add files via upload

--- ANOVA-Analysis.R
+++ ANOVA-Analysis.R
@@ -9,4 +9,11 @@
 >PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv"
 #Reading the CSV file in R
 >df<-read.csv(PATH)
->head(df)
+#Applying ANOVA test to columns time and poison
+>anova_one_way<-aov(time~poison,data=df)
+>summary(anova_one_way)
+# Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
